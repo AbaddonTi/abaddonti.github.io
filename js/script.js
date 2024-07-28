d0279f9bdca9a01d94ffffb2e8b26c02d38c1977
Grigorii Kulkov <145202297+AbaddonTi@users.noreply.github.com>
2024-07-28 21:49:54 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -157,10 +157,6 @@ function formatDate(dateString) {
     return date.toFormat('dd.MM.yyyy HH:mm');
 }
 
-function formatPercentage(value) {
-    return `${(parseFloat(value) * 100).toFixed(2).replace('.', ',')} %`;
-}
-
 function toggleExpense(button) {
     button.classList.toggle('active');
     updateDataDisplay();
@@ -205,10 +201,10 @@ function updateDataDisplay() {
                             <td>${formatDate(record['Дата'])}</td>
                             <td>${record['Сотрудник']}</td>
                             <td>${record['Операция']}</td>
-                            <td>${formatCurrency(record['Сумма'])} $</td>
-                            <td>${formatCurrency(record['Объем'])} $</td>
-                            <td>${formatCurrency(record['Профит'])} $</td>
-                            <td>${formatPercentage(record['Спред'])}</td>
+                            <td>${record['Сумма']}</td>
+                            <td>${record['Объем']}</td>
+                            <td>${record['Профит']}</td>
+                            <td>${record['Спред']}</td>
                         </tr>
                     `).join('')}
                 </tbody>
